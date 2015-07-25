af1796e07f224458161f563f7e9481e23c52aaaf
Chris Reynolds <chris@jazzsequence.com>
2015-07-25 14:55:04 -0600

    function to get the current this or that status and return the actual message

--- includes/tweet.php
+++ includes/tweet.php
@@ -33,6 +33,34 @@ class Twitterbot_Tweet {
 	 */
 	public function hooks() {
 	}
+
+	/**
+	 * Get the current this or that status and return an output string based on the result.
+	 *
+	 * @since  0.1.0
+	 * @return string
+	 */
+	public function get_this_or_that() {
+		$which = get_option( '_twitterbot_this_or_that' );
+
+		switch ( $which ) {
+			case 'prompt':
+				return __( 'This...or that?', 'twitterbot' );
+				break;
+
+			case 'this':
+				return __( 'This.', 'twitterbot' );
+				break;
+
+			case 'that':
+				return __( 'That.', 'twitterbot' );
+				break;
+
+			default:
+				return __( 'This...or that?', 'twitterbot' );
+				break;
+		}
+	}
 }
 
 function twitterbot_tweet() {
